f156986de990fb5294621f14bceb933d9851df46
Aeriech <aeriechancheta143@gmail.com>
2023-04-14 09:37:27 +0800

    modified:   resources/js/components/inventory/AddCategory.jsx 	modified:   resources/js/components/inventory/AddMeasurement.jsx

--- resources/js/components/inventory/AddCategory.jsx
+++ resources/js/components/inventory/AddCategory.jsx
@@ -1,98 +1,143 @@
-import React from 'react'
-import { Typography,TextField,Box,Grid
-, MenuItem, FormControl, Select, Button
-} from '@mui/material';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-
-
+import React from "react";
+import {
+    Typography,
+    TextField,
+    Box,
+    Grid,
+    MenuItem,
+    FormControl,
+    Select,
+    Button,
+} from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function AddCategory() {
     const navigate = useNavigate();
 
-    const [category, setCategory] = React.useState('1');
+    const [category, setCategory] = React.useState("1");
     const [subCategory, setSubCategory] = React.useState("");
 
-
     const handleCategory = (event) => {
-      setCategory(event.target.value);
+        setCategory(event.target.value);
     };
 
     const createCategory = async (e) => {
-  e.preventDefault();
+        e.preventDefault();
 
-  const formData = new FormData();
+        const formData = new FormData();
 
-  formData.append("category", category);
-  formData.append("subCategory", subCategory);
+        formData.append("category", category);
+        formData.append("subCategory", subCategory);
 
-  try {
-    const { data } = await axios.post("/api/add-category", formData);
-    toast.fire({
-      icon: "success",
-      title: "Category added successfully",
-    });
-    navigate("/");
-  } catch (error) {
-    if (error.response && error.response.status === 422) {
-      setErrors(error.response.data.errors);
-  } else {
-      // handle other errors here
-  }
-  }
-};
+        try {
+            const { data } = await axios.post("/api/add-category", formData);
+            toast.fire({
+                icon: "success",
+                title: "Category added successfully",
+            });
+            navigate("/");
+        } catch (error) {
+            if (error.response && error.response.status === 422) {
+                setErrors(error.response.data.errors);
+            } else {
+                // handle other errors here
+            }
+        }
+    };
 
-const [errors, setErrors] = useState(null);
+    const [errors, setErrors] = useState(null);
 
-  return (
-    <div > 
-      <Box sx={{ flexGrow: 1}}  textAlign="center" marginTop="20px">
+    const handleBack = () => {
+        navigate("/");
+    };
 
-      <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginRight:"15px", marginLeft:"15px" }}>
-  <Typography  variant='h4' style={{textAlign: "left"}}>Add Category</Typography>
-  
-  <div>
-  <Button size='large' variant="outlined" onClick={(event) => createCategory(event)}><Typography variant='h6'>SAVE</Typography></Button>
-  </div>
-</div>
-  
-<Box marginTop="10px">
-{errors && (
-                <div className="alert alert-danger">
-                    <ul>
-                        {Object.values(errors).map((messages, index) => (
-                            <li key={index}>{messages[0]}</li>
-                        ))}
-                    </ul>
+    return (
+        <div>
+            <Box sx={{ flexGrow: 1 }} textAlign="center" marginTop="20px">
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        marginRight: "15px",
+                        marginLeft: "15px",
+                    }}
+                >
+                    <Typography variant="h4" style={{ textAlign: "left" }}>
+                        Add Category
+                    </Typography>
+
+                    <div>
+                        <Button
+                            size="large"
+                            variant="outlined"
+                            onClick={handleBack}
+                        >
+                            <Typography variant="h6">HOME</Typography>
+                        </Button>
+                        <Button
+                            size="large"
+                            variant="outlined"
+                            onClick={(event) => createCategory(event)}
+                        >
+                            <Typography variant="h6">SAVE</Typography>
+                        </Button>
+                    </div>
                 </div>
-            )}
+
+                <Box marginTop="10px">
+                    {errors && (
+                        <div className="alert alert-danger">
+                            <ul>
+                                {Object.values(errors).map(
+                                    (messages, index) => (
+                                        <li key={index}>{messages[0]}</li>
+                                    )
+                                )}
+                            </ul>
+                        </div>
+                    )}
+                </Box>
+
+                <Box border={2} borderColor="black" padding={5} margin="10px">
+                    <Grid
+                        alignItems="center"
+                        container
+                        spacing={{ xs: 2, md: 3 }}
+                        columns={{ xs: 4, sm: 8, md: 12 }}
+                    >
+                        <Grid item xs={12} sm={6} md={6}>
+                            <Typography variant="h5">Main Category</Typography>
+                            <FormControl
+                                variant="filled"
+                                sx={{ m: 1, minWidth: 180 }}
+                            >
+                                <Select
+                                    labelId="demo-simple-select-filled-label"
+                                    id="demo-simple-select-filled"
+                                    value={category}
+                                    onChange={handleCategory}
+                                >
+                                    <MenuItem value="1">Perishable</MenuItem>
+                                    <MenuItem value="2">
+                                        Non-Perishable
+                                    </MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={6}>
+                            <Typography variant="h5">Sub Category</Typography>
+                            <TextField
+                                id="filled-basic"
+                                variant="filled"
+                                value={subCategory}
+                                onChange={(e) => setSubCategory(e.target.value)}
+                            />
+                        </Grid>
+                    </Grid>
+                </Box>
             </Box>
-
-  <Box border={2} borderColor="black" padding={5} margin="10px">
-    <Grid alignItems="center" container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} >
-      <Grid item xs={12} sm={6} md={6}>
-        <Typography variant='h5'>Main Category</Typography>
-        <FormControl variant="filled" sx={{ m: 1, minWidth: 180 }}>
-          <Select
-            labelId="demo-simple-select-filled-label"
-            id="demo-simple-select-filled"
-            value={category}
-            onChange={handleCategory}
-          >
-            <MenuItem value="1">Perishable</MenuItem>
-            <MenuItem value="2">Non-Perishable</MenuItem>
-          </Select>
-        </FormControl>
-      </Grid>
-      <Grid item xs={12} sm={6} md={6}>
-        <Typography variant='h5'>Sub Category</Typography>
-        <TextField id="filled-basic" variant="filled" value={subCategory} onChange={(e) => (setSubCategory(e.target.value))}/>
-      </Grid>
-    </Grid>
-  </Box>
-</Box>
-
-    </div>
-  )
+        </div>
+    );
 }
--- resources/js/components/inventory/AddMeasurement.jsx
+++ resources/js/components/inventory/AddMeasurement.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { Typography,TextField,Box,Grid
-, MenuItem, FormControl, Select, Button
-} from '@mui/material';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-
-
+import React from "react";
+import {
+    Typography,
+    TextField,
+    Box,
+    Grid,
+    MenuItem,
+    FormControl,
+    Select,
+    Button,
+} from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function AddMeasurement() {
     const navigate = useNavigate();
@@ -14,64 +18,101 @@ export default function AddMeasurement() {
     const [measurement, setMeasurement] = React.useState("");
 
     const createMeasurement = async (e) => {
-  e.preventDefault();
+        e.preventDefault();
 
-  const formData = new FormData();
+        const formData = new FormData();
 
-  formData.append("measurement", measurement);
+        formData.append("measurement", measurement);
 
-  try {
-    const { data } = await axios.post("/api/add-measurement", formData);
-    toast.fire({
-      icon: "success",
-      title: "Measurement added successfully",
-    });
-    navigate("/");
-  } catch (error) {
-    if (error.response && error.response.status === 422) {
-      setErrors(error.response.data.errors);
-  } else {
-      // handle other errors here
-  }
-  }
-};
+        try {
+            const { data } = await axios.post("/api/add-measurement", formData);
+            toast.fire({
+                icon: "success",
+                title: "Measurement added successfully",
+            });
+            navigate("/");
+        } catch (error) {
+            if (error.response && error.response.status === 422) {
+                setErrors(error.response.data.errors);
+            } else {
+                // handle other errors here
+            }
+        }
+    };
 
-const [errors, setErrors] = useState(null);
+    const [errors, setErrors] = useState(null);
 
-  return (
-    <div > 
-      <Box sx={{ flexGrow: 1}}  textAlign="center" marginTop="20px">
+    const handleBack = () => {
+        navigate("/");
+    };
 
-      <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginRight:"15px", marginLeft:"15px" }}>
-  <Typography  variant='h4' style={{textAlign: "left"}}>Add Measurement</Typography>
-  
-  <div>
-  <Button size='large' variant="outlined" onClick={(event) => createMeasurement(event)}><Typography variant='h6'>SAVE</Typography></Button>
-  </div>
-</div>
-  
-<Box marginTop="10px">
-{errors && (
-                <div className="alert alert-danger">
-                    <ul>
-                        {Object.values(errors).map((messages, index) => (
-                            <li key={index}>{messages[0]}</li>
-                        ))}
-                    </ul>
+    return (
+        <div>
+            <Box sx={{ flexGrow: 1 }} textAlign="center" marginTop="20px">
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        marginRight: "15px",
+                        marginLeft: "15px",
+                    }}
+                >
+                    <Typography variant="h4" style={{ textAlign: "left" }}>
+                        Add Measurement
+                    </Typography>
+
+                    <div>
+                        <Button
+                            size="large"
+                            variant="outlined"
+                            onClick={handleBack}
+                        >
+                            <Typography variant="h6">HOME</Typography>
+                        </Button>
+                        <Button
+                            size="large"
+                            variant="outlined"
+                            onClick={(event) => createMeasurement(event)}
+                        >
+                            <Typography variant="h6">SAVE</Typography>
+                        </Button>
+                    </div>
                 </div>
-            )}
+
+                <Box marginTop="10px">
+                    {errors && (
+                        <div className="alert alert-danger">
+                            <ul>
+                                {Object.values(errors).map(
+                                    (messages, index) => (
+                                        <li key={index}>{messages[0]}</li>
+                                    )
+                                )}
+                            </ul>
+                        </div>
+                    )}
+                </Box>
+
+                <Box border={2} borderColor="black" padding={5} margin="10px">
+                    <Grid
+                        alignItems="center"
+                        container
+                        spacing={{ xs: 2, md: 3 }}
+                        columns={{ xs: 4, sm: 8, md: 12 }}
+                    >
+                        <Grid item xs={12} sm={12} md={12}>
+                            <Typography variant="h5">Measurement</Typography>
+                            <TextField
+                                id="filled-basic"
+                                variant="filled"
+                                value={measurement}
+                                onChange={(e) => setMeasurement(e.target.value)}
+                            />
+                        </Grid>
+                    </Grid>
+                </Box>
             </Box>
-
-  <Box border={2} borderColor="black" padding={5} margin="10px">
-    <Grid alignItems="center" container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} >
-      <Grid item xs={12} sm={12} md={12}>
-        <Typography variant='h5'>Measurement</Typography>
-        <TextField id="filled-basic" variant="filled" value={measurement} onChange={(e) => (setMeasurement(e.target.value))}/>
-      </Grid>
-    </Grid>
-  </Box>
-</Box>
-
-    </div>
-  )
+        </div>
+    );
 }
