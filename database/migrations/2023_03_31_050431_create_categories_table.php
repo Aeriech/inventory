a1026bf586a754f0b728ef68cf622e1fd9049617
Aeriech <aeriechancheta143@gmail.com>
2023-04-17 14:35:17 +0800

    modified:   database/migrations/2023_03_31_050431_create_categories_table.php 	modified:   database/migrations/Measurement.php 	new file:   public/upload/1681711745.jpeg

--- database/migrations/2023_03_31_050431_create_categories_table.php
+++ database/migrations/2023_03_31_050431_create_categories_table.php
@@ -3,12 +3,14 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
     /**
      * Run the migrations.
      */
+
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
@@ -18,7 +20,16 @@ return new class extends Migration
             $table->foreign('parent_id')->references('id')->on('categories');
             $table->timestamps();
         });
+
+        // Insert default values
+        DB::table('categories')->insert([
+            ['category' => 'Perishable'],
+            ['category' => 'Non-Perishable'],
+            ['category' => 'Vegetable', 'parent_id' => 1],
+            ['category' => 'Utensil', 'parent_id' => 2],
+        ]);
     }
+
 
     /**
      * Reverse the migrations.
--- database/migrations/Measurement.php
+++ database/migrations/Measurement.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -16,6 +17,14 @@ return new class extends Migration
             $table->string('measurement', 255)->nullable();
             $table->timestamps();
         });
+
+        // Insert default values
+        DB::table('measurements')->insert([
+            ['measurement' => 'Quantity'],
+            ['measurement' => 'Kilogram'],
+            ['measurement' => 'Liters'],
+
+        ]);
     }
 
     /**
